dbaff3119621fc905ed0fdd32ea435a13099352a
Joan Salas <joan.salas.silva@gmail.com>
2023-05-29 12:58:34 -0400

    generar routing module basico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 // Definicion de rutas
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent,
   },
   {
     path: 'about',
     component: AboutPageComponent,
   },
+  {
+    path: 'contact',
+    component: ContactPageComponent,
+  },
   // Hacer redireccion
   {
     // Ruta comodin
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '',
   },
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, HomePageComponent],
-  imports: [CommonModule],
-  exports: [AboutPageComponent, HomePageComponent],
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+  ],
 })
 export class SharedModule {}
