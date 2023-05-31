05277a32863d9317a44d546802d85d1b2f05de81
Joan Salas <joan.salas.silva@gmail.com>
2023-05-31 17:57:45 -0400

    hacer peticion get y mostrar datos en tabla

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -6,8 +8,12 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+  constructor(private countriesService: CountriesService) {}
+
   searchByCapital(term: string): void {
-    console.log('Desde by capital page');
-    console.log({ term });
+    this.countriesService.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
   }
 }
