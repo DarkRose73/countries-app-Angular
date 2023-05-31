786dc8602f694d93b166799c92976daa7d98ba03
Joan Salas <joan.salas.silva@gmail.com>
2023-05-31 15:05:18 -0400

    countries routing module

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountriesRoutingModule } from './countries-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    CountryPageComponent,
+    ByRegionPageComponent,
+  ],
+  imports: [CommonModule, CountriesRoutingModule],
 })
-export class CountriesModule { }
+export class CountriesModule {}
