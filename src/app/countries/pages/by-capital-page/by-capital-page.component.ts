a55f7257cfb8de5f476b759cc84ab3fe179efbed
Joan Salas <joan.salas.silva@gmail.com>
2023-05-31 16:36:02 -0400

    funcionamiento searchBox

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCapitalPageComponent {
-
+  searchByCapital(term: string): void {
+    console.log('Desde by capital page');
+    console.log({ term });
+  }
 }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -8,4 +8,11 @@ import { Component, Input } from '@angular/core';
 export class SearchBoxComponent {
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  public emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
 }
