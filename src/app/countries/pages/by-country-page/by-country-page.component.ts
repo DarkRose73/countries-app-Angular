d05e570249f75c8c6545bf28a5d0b12e7fb66407
Joan Gabriel Salas Silva <82409450+DarkRose73@users.noreply.github.com>
2023-06-06 11:24:42 -0400

    busqueda por region y pais

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCountryPageComponent {
+  public countries: Country[] = [];
 
+  constructor(private countriesService: CountriesService) {}
+
+  searchByCountry(term: string): void {
+    this.countriesService.searchCountry(term).subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
 }
